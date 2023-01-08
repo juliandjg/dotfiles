b633bb5f92bdfcc0914c88812fd91e8757378751
Julián De Juan García <82332809+juliandjg@users.noreply.github.com>
2023-01-08 16:10:01 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,14 +4,16 @@ sudo apt-get update -y && sudo apt-get full-upgrade -y
 
 sudo apt-get install -y \
     zsh zoxide exa unzip zip bat \
-    ca-certificates curl gnupg lsb-release
+    ca-certificates curl gnupg lsb-release \
+    znap
 
 # Fix batcat -> bat
 sudo ln -s /usr/bin/batcat /usr/local/bin/bat
 
 # Oh-my-zsh
+cat $HOME/.dotfiles/.zshrc > ~/.zshrc
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-cat $HOME/.dotfiles/.zshrc >> ~/.zshrc
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 # Starship
 curl -sS https://starship.rs/install.sh | sh
@@ -30,6 +32,11 @@ curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o 
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sudo apt-get update -y && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo apt-get update -y && sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 sudo apt autoremove -y
+
+# Change to ZSH
+sudo chsh -s /usr/bin/zsh
+
+zsh
